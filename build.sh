8bfa17c974b487598af6d86ce1469d9225f98ce4
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2024-08-08 10:00:56 +0100

    Updated boilerplate

---
+++ build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Check for work folder specified
+if [ $# -ge 1 ]
+then
+  workdir=$1
+  echo "Entering ${workdir}"
+  cd "${workdir}"
+fi
+
+if [ $# -ge 2 ]
+then
+  param=$2
+else
+  param=""
+fi
+
+zipfile="js13k.zip"
+buildpath="tmpbuild"
+jscat="${buildpath}/min.js"
+indexcat="${buildpath}/index.html"
+
+if [ "${param}" == "run" ]
+then
+  curbrowser=`which xdg-open >/dev/null 2>&1`
+  if [ "${curbrowser}" == "" ]
+  then
+    curbrowser="firefox"
+  fi
+
+  ${curbrowser} "index.html" >/dev/null 2>&1
+  exit 0
+fi
+
+# Create clean build folder
+echo "Cleaning build folder"
+rm -Rf "${buildpath}" >/dev/null 2>&1
+mkdir "${buildpath}"
+
+# Concatenate the JS files
+echo "Concatenating JS"
+touch "${jscat}" >/dev/null 2>&1
+for file in "main.js"
+do
+  cat "${file}" >> "${jscat}"
+done
+
+# Add the index header
+echo -n '<!DOCTYPE html><html><head><meta charset="utf-8"/><meta http-equiv="Content-Type" content="text/html;charset=utf-8"/><title>JS13k 2023</title><style>' > "${indexcat}"
+
+# Inject the concatenated and minified CSS files
+echo "Minifying CSS"
+for file in "main.css"
+do
+  JAVA_CMD=java yui-compressor "${file}" >> "${indexcat}"
+done
+
+# Add on the rest of the index file
+echo -n '</style><script type="text/javascript">' >> "${indexcat}"
+
+# Inject the closure-ised and minified JS
+echo "Using closure to minify JS"
+./closeyoureyes.sh "${jscat}" | tr -d '\n' >> "${indexcat}"
+
+# Add on the rest of the index file
+echo -n '</script><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/></head><body><div id="wrapper"><canvas id="canvas" width="640" height="360"></canvas></div></body></html>' >> "${indexcat}"
+
+# Remove the minified JS
+rm "${jscat}" >/dev/null 2>&1
+
+# Remove old zip
+rm -Rf "${zipfile}" >/dev/null 2>&1
+
+# Zip everything up
+echo "ZIP build folder"
+zip -j "${zipfile}" "${buildpath}"/*
+
+# Re-Zip with advzip to save a bit more
+echo "Trying to reduce ZIP size"
+advzip -i 200 -k -z -4 "${zipfile}"
+
+# Determine file sizes and compression
+unzip -lv "${zipfile}"
+stat "${zipfile}"
+
+zipsize=`stat -c %s "${zipfile}"`
+maxsize=$((13*1024))
+bytesleft=$((${maxsize}-${zipsize}))
+percent=$((200*${zipsize}/${maxsize} % 2 + 100*${zipsize}/${maxsize}))
+
+if [ ${bytesleft} -ge 0 ]
+then
+  echo "YAY ${percent}% used - it fits with ${bytesleft} bytes spare"
+else
+  echo "OH NO ${percent}% used - it's gone ovey by "$((0-${bytesleft}))" bytes"
+fi
---
+++ main.js
@@ -0,0 +1,171 @@
+// JS 13k 2024 entry
+
+// Global constants
+const xmax=640;
+const ymax=360;
+
+// Game state
+var gs={
+  // Canvas
+  canvas:null,
+  ctx:null,
+  scale:1,
+
+  // Main character
+  keystate:0,
+  x:0, // x position
+  y:0, // y position
+  vs:0, // vertical speed
+  hs:0, // horizontal speed
+  jump:false, // jumping
+  fall:false, // falling
+  dir:0, //direction (-1=left, 0=none, 1=right)
+
+  debug:false
+};
+
+// Clear keyboard input state
+function clearinputstate()
+{
+  gs.keystate=0;
+}
+
+// Check if an input is set in keyboard input state
+function ispressed(keybit)
+{
+  return ((gs.keystate&keybit)!=0);
+}
+
+// Handle resize events
+function playfieldsize()
+{
+  var height=window.innerHeight;
+  var aspectratio=xmax/ymax;
+  var ratio=xmax/ymax;
+  var width=Math.floor(height*ratio);
+  var top=0;
+  var left=Math.floor((window.innerWidth/2)-(width/2));
+
+  if (width>window.innerWidth)
+  {
+    width=window.innerWidth;
+    ratio=ymax/xmax;
+    height=Math.floor(width*ratio);
+
+    left=0;
+    top=Math.floor((window.innerHeight/2)-(height/2));
+  }
+
+  gs.scale=(height/ymax);
+
+  gs.canvas.style.top=top+"px";
+  gs.canvas.style.left=left+"px";
+  gs.canvas.style.transformOrigin='0 0';
+  gs.canvas.style.transform='scale('+gs.scale+')';
+}
+
+// Update the player key state
+function updatekeystate(e, dir)
+{
+  switch (e.which)
+  {
+    case 37: // cursor left
+    case 65: // A
+    case 90: // Z
+      if (dir==1)
+        gs.keystate|=1;
+      else
+        gs.keystate&=~1;
+
+      e.preventDefault();
+      break;
+
+    case 38: // cursor up
+    case 87: // W
+    case 59: // semicolon
+      if (dir==1)
+        gs.keystate|=2;
+      else
+        gs.keystate&=~2;
+
+      e.preventDefault();
+      break;
+
+    case 39: // cursor right
+    case 68: // D
+    case 88: // X
+      if (dir==1)
+        gs.keystate|=4;
+      else
+        gs.keystate&=~4;
+
+      e.preventDefault();
+      break;
+
+    case 40: // cursor down
+    case 83: // S
+    case 190: // dot
+      if (dir==1)
+        gs.keystate|=8;
+      else
+        gs.keystate&=~8;
+
+      e.preventDefault();
+      break;
+
+    case 13: // enter
+    case 32: // space
+      if (dir==1)
+        gs.keystate|=16;
+      else
+        gs.keystate&=~16;
+
+      e.preventDefault();
+      break;
+
+    case 73: // I (for info/debug)
+      if (dir==1)
+        gs.debug=(!gs.debug);
+
+      e.preventDefault();
+      break;
+
+    default:
+      break;
+  }
+}
+
+// Entry point
+function init()
+{
+  // Initialise stuff
+  document.onkeydown=function(e)
+  {
+    e = e || window.event;
+    updatekeystate(e, 1);
+  };
+
+  document.onkeyup=function(e)
+  {
+    e = e || window.event;
+    updatekeystate(e, 0);
+  };
+
+  // Stop things from being dragged around
+  window.ondragstart=function(e)
+  {
+    e = e || window.event;
+    e.preventDefault();
+  };
+
+  // Set up canvas
+  gs.canvas=document.getElementById("canvas");
+  gs.ctx=gs.canvas.getContext("2d");
+
+  window.addEventListener("resize", function() { playfieldsize(); });
+
+  playfieldsize();
+}
+
+// Run the init() once page has loaded
+window.onload=function() { init(); };
