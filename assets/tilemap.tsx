1abf530eea31287bbbb674e272f8fc3c8232e1df
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2024-09-11 22:37:25 +0100

    Added NPC spawn point to tileset

--- assets/tilemap.tsx
+++ assets/tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilemap_2024" tilewidth="8" tileheight="8" tilecount="6" columns="6">
- <image source="tilemap.png" width="48" height="8"/>
+<tileset name="tilemap_2024" tilewidth="8" tileheight="8" tilecount="7" columns="7">
+ <image source="tilemap.png" width="56" height="8"/>
 </tileset>
