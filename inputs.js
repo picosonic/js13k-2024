9d4b590022b364aad4f62061dcd4a8e51448742a
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2026-06-18 17:21:21 +0100

    Fix typo and change threshold

--- inputs.js
+++ inputs.js
@@ -168,10 +168,10 @@ function tilting(e)
   {
     gs.keystate=KEYNONE;
 
-    if (e.beta<-50)
-      gs.keystate|=LEFT;
+    if (e.beta<-45)
+      gs.keystate|=KEYLEFT;
 
-    if (e.beta>50)
+    if (e.beta>45)
       gs.keystate|=KEYRIGHT;
 
     if (e.gamma<-30)
